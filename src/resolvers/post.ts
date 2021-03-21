83e3b8e1e12c63b6ff78a04decedd01df8c8974a
Di <elven.legacy@hotmail.com>
2021-03-21 23:02:18 +0300

    Запрос конкретного поста

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,15 +1,22 @@
-import {Resolver, Query, Ctx} from "type-graphql";
-import { Post } from "../entities/Post";
+import {Resolver, Query, Ctx, Arg, Int} from "type-graphql";
+import {Post} from "../entities/Post";
 import {MyContext} from "../types";
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
     posts(
-     @Ctx() ctx: MyContext
-    ){
-        console.log('---------')
-        console.log(ctx);
+        @Ctx() ctx: MyContext
+    ): Promise<Post[]> {
         return ctx.em.find(Post, {});
     }
+
+    @Query(() => Post, {nullable: true})
+    post(
+        @Arg('id', ()=> Int) id: number,
+        @Ctx() ctx: MyContext
+    ): Promise<Post | null> {
+        return ctx.em.findOne(Post, { id });
+    }
+
 }
